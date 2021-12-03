8d8c4470f61475c4c807f82acc0b1f2e89b60b19
buusc1 <buuscw@midco.net>
2021-12-02 19:42:03 -0600

    weooeheohwrhuwerhiweriwyfhifhl

--- PosRudeTimeNew/PosRudeTimeNew/Calender.cs
+++ PosRudeTimeNew/PosRudeTimeNew/Calender.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PosRudeTimeNew
 {
-    public partial class Form2 : Form
+    public partial class Calender : Form
     {
-        public Form2()
+        public Calender()
         {
             InitializeComponent();
         }
