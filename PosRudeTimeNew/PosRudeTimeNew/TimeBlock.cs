61c3e93fbe91059ade68fc8aa1c0a9d2cb6ada20
sbfffnico <nicoedrich91@gmail.com>
2021-12-08 21:56:34 -0600

    Added all methods to TimeBlock.cs Installed Newtonsoft.json Added System.IO to namespaces

--- PosRudeTimeNew/PosRudeTimeNew/TimeBlock.cs
+++ PosRudeTimeNew/PosRudeTimeNew/TimeBlock.cs
@@ -1,33 +1,76 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PosRudeTimeNew
 {
-    class TimeBlock
+    public class TimeBlock
     {
-        public static string Name;
-        public static string Description;
-        public static string Location;
-        public static DateTime StartTime;
-        public static DateTime EndTime;
-        TimeBlock(string name, string description, string location, DateTime startime, DateTime endtime)
-        {
-            Name = name;
-            Description = description;
-            Location = location; 
-            StartTime = startime;
-            EndTime = endtime;
-        }
-        public void Add()
-        {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
 
-        }
-        public void Remove()
+        public static void Serialize(List<TimeBlock> timeBlock)
         {
+            string username = Environment.UserName;
+            // folder name will need to be fixed
+            string fileName = @"C:\Users\" + username + "\\source\\repos_new_12082021\\PosRudeTimeNew\\RudeTimeDates.json";
 
+            //var options = new JsonSerializerOptions { WriteIndented = true };
+            //string jsonString = JsonSerializer.Serialize(timeBlock, options);
+            File.WriteAllText(fileName, JsonConvert.SerializeObject(timeBlock, Formatting.Indented));
         }
+
+        public static List<TimeBlock> Deserialize()
+        {
+            var timeBlock = new List<TimeBlock>();
+
+            string username = Environment.UserName;
+            // folder name here will need to be fixed
+            string fileName = @"C:\Users\" + username + "\\source\\repos_new_12082021\\PosRudeTimeNew\\RudeTimeDates.json";
+            //string jsonString = File.ReadAllText(fileName);
+            timeBlock = JsonConvert.DeserializeObject<List<TimeBlock>>(File.ReadAllText(fileName));
+
+            return timeBlock;
+        }
+
+        public static void AddBlock(List<TimeBlock> timeBlock, DateTime startTime, DateTime endTime, string name, string location, string description)
+        {
+            timeBlock.Add(new TimeBlock
+            {
+                StartTime = startTime,
+                EndTime = endTime,
+                Name = name,
+                Location = location,
+                Description = description
+            });
+
+            Serialize(timeBlock);
+        }
+
+        public static void DeleteBlock(List<TimeBlock> timeBlock, DateTime editDate)
+        {
+            var block = timeBlock.SingleOrDefault(date => date.StartTime == editDate);
+            if (block != null)
+            {
+                timeBlock.Remove(block);
+            }
+
+            Serialize(timeBlock);
+        }
+
+        public static void EditBlock(List<TimeBlock> timeBlock, DateTime editItem, DateTime startTime, DateTime endTime, string name, string location, string description)
+        {
+            DeleteBlock(timeBlock, editItem);
+            AddBlock(timeBlock, startTime, endTime, name, location, description);
+        }
+
     }
 }
+
